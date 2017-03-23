40014b63c0306699049873a35bf148c0449ad2d5
Juan Esteban Molano <je.molano1498@uniandes.edu.co>
2017-03-23 13:10:28 -0500

    Update HospitalLosAlpesDB.java

--- src/main/java/Utilities/HospitalLosAlpesDB.java
+++ src/main/java/Utilities/HospitalLosAlpesDB.java
@@ -10,7 +10,7 @@ import com.mongodb.MongoClientURI;
 
 public class HospitalLosAlpesDB {
 	
-	private static String MONGO__URI = "mongodb://localhost:27017"; 
+	private static String MONGO_URI = "mongodb://172.24.42.74:27017"; 
 	private static String MONGO__DB = "hospitallosaples";
 	
 	
@@ -59,7 +59,7 @@ public class HospitalLosAlpesDB {
 
 
 	//private static String MONGO_URI = "mongodb://elcrack:masterdb@ds145329.mlab.com:45329/hospitallosaples"; 
-	private static String MONGO_URI = "mongodb://localhost:27017";
+	private static String MONGO__URI = "mongodb://localhost:27017";
 	private static String MONGO_DB = "hospitallosaples";
 
 }
